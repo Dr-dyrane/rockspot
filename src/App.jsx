1389030e4c8062c5074287d5da624fcfac2df531
Dr-dyrane <halodyrane@gmail.com>
2023-12-22 22:37:25 +0100

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/appRoutes";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return (
-    <Router>
-      <div className="bg-slate-200 min-h-screen">
-        <Navbar />
-        <div className="flex justify-center items-center text-indigo-700 font-bold text-center">
-          <AppRoutes />
-        </div>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="bg-slate-200 min-h-screen flex flex-col">
+				<Navbar />
+				<div className="flex-1 justify-center items-center text-indigo-700 font-bold text-center overflow-scroll">
+					<AppRoutes />
+				</div>
+				<Footer />
+			</div>
+		</Router>
+	);
 };
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="bg-white rounded-lg shadow m-1 dark:bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © {currentYear}{" "}
+          <Link to="/" className="hover:underline">
+            Rockspot™
+          </Link>
+          . All Rights Reserved.
+        </span>
+        <div className="flex items-center mt-3 text-xs font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <img
+            src="/dyrane.png"  // Replace with the actual path to your logo
+            alt="Logo"
+            className="h-5 w-auto"
+          />
+          <a
+            href="https://linktr.ee/Dyrane"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline ml-4"
+          >
+            Designed by Dyrane.
+          </a>
+        </div>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <Link to="#" className="hover:underline me-4 md:me-6">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="hover:underline me-4 md:me-6">
+              Privacy Policy
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="hover:underline me-4 md:me-6">
+              Licensing
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="hover:underline">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
