eb683522bad1711ba50364935096bd554a5b7b76
Dr-dyrane <halodyrane@gmail.com>
2023-12-23 23:34:50 +0100

    dynamic rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,25 +1,34 @@
 // src/components/Rating.jsx
 import React from "react";
-import { FaStar } from "react-icons/fa6";
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 
 const Rating = ({ rating, reviews }) => {
-  // Round the rating to the nearest half
-  const roundedRating = Math.round(rating * 2) / 2;
+  const integerPart = Math.floor(rating);
+  const decimalPart = rating % 1;
+  const stars = [];
+
+  // Fill the integer part
+  for (let i = 0; i < integerPart; i++) {
+    stars.push(<FaStar key={i} size={18} color="yellow" />);
+  }
+
+  // Fill the decimal part
+  if (decimalPart > 0) {
+    stars.push(<FaStarHalfAlt key={integerPart} size={18} color="yellow" />);
+  }
+
+  // Add empty stars if needed
+  const remainingStars = 5 - stars.length;
+  for (let i = 0; i < remainingStars; i++) {
+    stars.push(<FaStar key={integerPart + i + 1} size={18} color="gray" />);
+  }
 
   return (
     <div className="flex items-start flex-col">
-      <div className="flex flex-rw">
-        {[...Array(5)].map((_, index) => (
-          <FaStar
-            key={index}
-            size={18}
-            color={index + 0.5 <= roundedRating ? "yellow" : "gray"}
-          />
-        ))}
-      </div>
+      <div className="flex">{stars}</div>
       <div className="flex flex-row items-center justify-center mt-2">
         <p className="text-sm font-bold text-gray-900 dark:text-white">
-          {roundedRating}
+          {rating}
         </p>
         <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
         <a
