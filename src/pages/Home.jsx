a3d52a8a4ba12f6aadd68b5fc8abed17b4d4dfce
Dr-dyrane <halodyrane@gmail.com>
2023-12-23 17:19:12 +0100

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
 
 			<div className="mt-8">
 				<h2 className="text-2xl font-semibold mb-4">Categories</h2>
-				<p className="text-sm mb-4 opacity-80 font-medium italic">
+				<p className="text-xs sm:text-sm mb-4 opacity-80 font-medium italic">
 					Explore a variety of mouth-watering shawarmas in different categories.
 				</p>
 				<div className="justify-center items-center p-1 grid grid-cols-5 md:grid-cols-6 lg:grid-cols-7 xl:grid-cols-8 gap-[8.5rem] mt-4 max-w-sm sm:max-w-3xl md:max-w-4xl lg:max-w-5xl xl:max-w-6xl overflow-x-scroll hide-scroll-bar">
@@ -27,7 +27,7 @@ const Home = () => {
 
 			<div className="flex flex-col items-center justify-center mt-8">
 				<h2 className="text-2xl font-semibold mb-4">Featured Shawarmas</h2>
-				<p className="text-sm opacity-80 mb-4 font-medium italic">
+				<p className="text-xs m:text-sm opacity-80 mb-4 font-medium italic">
 					Indulge in our signature shawarmas, carefully crafted for a delightful
 					experience.
 				</p>
@@ -35,7 +35,7 @@ const Home = () => {
 					{featuredShawarmas.map((shawarma) => (
 						<div
 							key={shawarma.id}
-							className="inline-block p-2 h-64 hover:animate-pulse"
+							className="inline-block p-2 ml-56 sm:ml-auto h-64 hover:animate-pulse"
 						>
 							{/* Added 'animate-bounce' for subtle bouncing animation */}
 							<ShawarmaCard shawarma={shawarma} />
