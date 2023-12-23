fde7c70bda5c7ec8cd8698e7e4a2994e20fcf834
Dr-dyrane <halodyrane@gmail.com>
2023-12-23 11:41:35 +0100

    add react icon - remove svg

--- src/components/Navbar/MenuButton.jsx
+++ src/components/Navbar/MenuButton.jsx
@@ -1,7 +1,8 @@
 // MenuButton.js
 import React from "react";
+import { BsFillMenuButtonFill, BsFillMenuButtonWideFill } from "react-icons/bs";
 
-const MenuButton = ({ isMenuOpen, toggleMenu}) => {
+const MenuButton = ({ isMenuOpen, toggleMenu }) => {
 	return (
 		<button
 			data-collapse-toggle="navbar-cta"
@@ -13,37 +14,9 @@ const MenuButton = ({ isMenuOpen, toggleMenu}) => {
 		>
 			<span className="sr-only">Open main menu</span>
 			{isMenuOpen ? (
-				<svg
-					className="w-5 h-5"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 17 14"
-				>
-					<path
-						stroke="currentColor"
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						strokeWidth="2"
-						d="M1 1h15M1 7h15M1 13h15"
-					/>
-				</svg>
+				<BsFillMenuButtonWideFill size={20} />
 			) : (
-				<svg
-					className="w-5 h-5"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						strokeWidth="2"
-						d="M4 6h16M4 12h16m-7 6h7"
-					/>
-				</svg>
+				<BsFillMenuButtonFill size={19} />
 			)}
 		</button>
 	);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,10 @@ import React from "react";
 const Home = () => {
   return (
     <div className="p-4 flex flex-col items-center justify-center">
-      <h2>Welcome to Rockspot Shawarma!</h2>
-      <img src='/logo.png' className="w-32 h-32 sm:w-48 sm:h-48 md:w-64 md:h-64" alt="Logo"/>
-      <p>Discover the flavors that will spice up your moment.</p>
+        {/* location row: image, deliver now */}
+        {/* search bar: sharwarma :menu search */}
+        {/* scrollview: categories: category card */}
+        {/* row:scrollview: featuredcategories: shawarma card */}
     </div>
   );
 };
