95b3fb3283568ad8e66e23d3ee1b4287027a8e3a
Dr-dyrane <halodyrane@gmail.com>
2023-12-23 10:47:38 +0100

    new file:   src/components/Navbar/MenuButton.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Menu from "./Menu";
 import { menuItems, userDropdownItems } from "./navbarData";
 import UserSection from "./UserSection";
+import MenuButton from "./MenuButton";
 
 const Navbar = () => {
 	const [isMenuOpen, setMenuOpen] = useState(false);
@@ -63,49 +64,7 @@ const Navbar = () => {
 						userDropdownRef={userDropdownRef}
 						userDropdownItems={userDropdownItems}
 					/>
-					<button
-						data-collapse-toggle="navbar-cta"
-						type="button"
-						className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-slate-500 rounded-lg md:hidden hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-200 dark:text-slate-400 dark:hover:bg-slate-700 dark:focus:ring-slate-600"
-						aria-controls="navbar-cta"
-						aria-expanded="false"
-						onClick={toggleMenu}
-					>
-						<span className="sr-only">Open main menu</span>
-						{isMenuOpen ? (
-							<svg
-								className="w-5 h-5"
-								aria-hidden="true"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 17 14"
-							>
-								<path
-									stroke="currentColor"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="M1 1h15M1 7h15M1 13h15"
-								/>
-							</svg>
-						) : (
-							<svg
-								className="w-5 h-5"
-								aria-hidden="true"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="M4 6h16M4 12h16m-7 6h7"
-								/>
-							</svg>
-						)}
-					</button>
+					<MenuButton isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
 				</div>
 				<div
 					className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
