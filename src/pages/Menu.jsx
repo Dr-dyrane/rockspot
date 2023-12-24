cb31107cdd5c7af7602a5149e31ddabda2268682
Dr-dyrane <halodyrane@gmail.com>
2023-12-24 22:44:19 +0100

    modified:   src/pages/Menu.jsx

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -32,7 +32,7 @@ const Menu = () => {
 			{/* Featured Shawarmas */}
 			<section>
 				<h2 className="text-2xl font-bold mb-4">Featured Shawarmas</h2>
-				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
 					{featuredShawarmas.map((shawarma) => (
 						<div key={shawarma.id}>
 							<MenuCard shawarma={shawarma} />
