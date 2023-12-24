9d5990680826c3cbea5ea63fac8c939b5841834d
Dr-dyrane <halodyrane@gmail.com>
2023-12-24 23:10:50 +0100

    modified:   src/pages/Menu.jsx

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -31,7 +31,9 @@ const Menu = () => {
 
 			{/* Featured Shawarmas */}
 			<section>
-				<h2 className="text-2xl font-bold mb-4">Featured Shawarmas</h2>
+				<h2 className="text-3xl font-extrabold mb-6 text-indigo-700">
+					Signature Shawarmas That Define Excellence
+				</h2>
 				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
 					{featuredShawarmas.map((shawarma) => (
 						<div key={shawarma.id} className="p-1.5">
