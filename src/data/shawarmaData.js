186298b0e21aa4054c70c85236354f6f37e4b4cc
Dr-dyrane <halodyrane@gmail.com>
2023-12-24 14:24:21 +0100

    formatting

--- src/data/shawarmaData.js
+++ src/data/shawarmaData.js
@@ -1,90 +1,89 @@
 // src/data/shawarmaData.js
 const categories = [
-    {
-      id: 1,
-      name: "Chicken",
-      image:
-        "https://images.pexels.com/photos/4728230/pexels-photo-4728230.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 2,
-      name: "Beef",
-      image:
-        "https://images.pexels.com/photos/7613568/pexels-photo-7613568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 3,
-      name: "Combo",
-      image:
-        "https://images.pexels.com/photos/6416559/pexels-photo-6416559.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 4,
-      name: "Kebab",
-      image:
-        "https://images.pexels.com/photos/6416559/pexels-photo-6416559.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 5,
-      name: "Other Beef",
-      image:
-        "https://images.pexels.com/photos/7613568/pexels-photo-7613568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 6,
-      name: "Veggies",
-      image:
-        "https://images.pexels.com/photos/9980749/pexels-photo-9980749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    // Add more categories as needed
-  ];
-  
-  const featuredShawarmas = [
-    {
-      id: 1,
-      name: "Spicy Chicken Shawarma",
-      price: 1500,
-      image:
-        "https://images.pexels.com/photos/4728230/pexels-photo-4728230.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 2,
-      name: "Classic Beef Shawarma",
-      price: 1500,
-      image:
-        "https://images.pexels.com/photos/7613568/pexels-photo-7613568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 3,
-      name: "Chicken Beef Shawarma",
-      price: 2000,
-      image:
-        "https://images.pexels.com/photos/6416559/pexels-photo-6416559.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-        id: 4,
-        name: "Spicy Chicken Shawarma",
-        price: 1500,
-        image:
-          "https://images.pexels.com/photos/4728230/pexels-photo-4728230.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      },
-      {
-        id: 5,
-        name: "Spicy Chicken Shawarma",
-        price: 1500,
-        image:
-          "https://images.pexels.com/photos/4728230/pexels-photo-4728230.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      },
-      {
-        id: 6,
-        name: "Vegetable Shawarma",
-        price: 1500,
-        image:
-          "https://images.pexels.com/photos/9980749/pexels-photo-9980749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      },
-    // Ad
-    // Add more featured shawarmas as needed
-  ];
-  
-  export { categories, featuredShawarmas };
-  
+	{
+		id: 1,
+		name: "Chicken",
+		image:
+			"https://images.pexels.com/photos/4728230/pexels-photo-4728230.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+	},
+	{
+		id: 2,
+		name: "Beef",
+		image:
+			"https://images.pexels.com/photos/7613568/pexels-photo-7613568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+	},
+	{
+		id: 3,
+		name: "Combo",
+		image:
+			"https://images.pexels.com/photos/6416559/pexels-photo-6416559.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+	},
+	{
+		id: 4,
+		name: "Kebab",
+		image:
+			"https://images.pexels.com/photos/6416559/pexels-photo-6416559.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+	},
+	{
+		id: 5,
+		name: "Other Beef",
+		image:
+			"https://images.pexels.com/photos/7613568/pexels-photo-7613568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+	},
+	{
+		id: 6,
+		name: "Veggies",
+		image:
+			"https://images.pexels.com/photos/9980749/pexels-photo-9980749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+	},
+	// Add more categories as needed
+];
+
+const featuredShawarmas = [
+	{
+		id: 1,
+		name: "Spicy Chicken Shawarma",
+		price: 1500,
+		image:
+			"https://images.pexels.com/photos/4728230/pexels-photo-4728230.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+	},
+	{
+		id: 2,
+		name: "Classic Beef Shawarma",
+		price: 1500,
+		image:
+			"https://images.pexels.com/photos/7613568/pexels-photo-7613568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+	},
+	{
+		id: 3,
+		name: "Chicken Beef Shawarma",
+		price: 2000,
+		image:
+			"https://images.pexels.com/photos/6416559/pexels-photo-6416559.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+	},
+	{
+		id: 4,
+		name: "Spicy Chicken Shawarma",
+		price: 1500,
+		image:
+			"https://images.pexels.com/photos/4728230/pexels-photo-4728230.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+	},
+	{
+		id: 5,
+		name: "Spicy Chicken Shawarma",
+		price: 1500,
+		image:
+			"https://images.pexels.com/photos/4728230/pexels-photo-4728230.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+	},
+	{
+		id: 6,
+		name: "Vegetable Shawarma",
+		price: 1500,
+		image:
+			"https://images.pexels.com/photos/9980749/pexels-photo-9980749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+	},
+	// Ad
+	// Add more featured shawarmas as needed
+];
+
+export { categories, featuredShawarmas };
