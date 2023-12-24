74e85f2e389f3ab57f2e1dbb671c0b7955228cf6
Dr-dyrane <halodyrane@gmail.com>
2023-12-24 03:45:31 +0100

    modified:   src/components/BottomNavbar.jsx

--- src/components/BottomNavbar.jsx
+++ src/components/BottomNavbar.jsx
@@ -1,22 +1,25 @@
 // src/components/BottomNavbar.jsx
 import React from "react";
-import { Link } from "react-router-dom";
-import { AiOutlineHome, AiOutlineSearch, AiOutlineShopping, AiOutlineQuestionCircle, AiOutlineUser } from 'react-icons/ai';
+import { Link, useLocation } from "react-router-dom";
+import { RiSearchFill, RiUser3Fill  } from "react-icons/ri";
+import { AiOutlineHome, AiFillHome, AiOutlineSearch, AiOutlineShopping, AiFillShopping, AiOutlineQuestionCircle, AiFillQuestionCircle, AiOutlineUser } from 'react-icons/ai';
 
 const navItems = [
-  { icon: <AiOutlineHome size={24} />, text: "Home", to: "/" },
-  { icon: <AiOutlineSearch size={24} />, text: "Search", to: "/search" },
-  { icon: <AiOutlineShopping size={24} />, text: "Orders", to: "/orders" },
-  { icon: <AiOutlineQuestionCircle size={24} />, text: "Support", to: "/support" },
-  { icon: <AiOutlineUser size={24} />, text: "Profile", to: "/profile" },
+  { outlineIcon: <AiOutlineHome size={24} />, filledIcon: <AiFillHome size={24} />, text: "Home", to: "/" },
+  { outlineIcon: <AiOutlineSearch size={24} />, filledIcon: <RiSearchFill size={24} />, text: "Search", to: "/search" },
+  { outlineIcon: <AiOutlineShopping size={24} />, filledIcon: <AiFillShopping size={24} />, text: "Orders", to: "/orders" },
+  { outlineIcon: <AiOutlineQuestionCircle size={24} />, filledIcon: <AiFillQuestionCircle size={24} />, text: "Support", to: "/support" },
+  { outlineIcon: <AiOutlineUser size={24} />, filledIcon: <RiUser3Fill  size={24} />, text: "Profile", to: "/profile" },
 ];
 
 const BottomNavbar = () => {
+  const location = useLocation();
+
   return (
     <nav className="bg-white border-t border-slate-200 fixed bottom-0 left-0 right-0 flex justify-around py-2">
       {navItems.map((item) => (
         <Link key={item.to} to={item.to} className="flex flex-col items-center text-indigo-700">
-          {item.icon}
+          {location.pathname === item.to ? item.filledIcon : item.outlineIcon}
           <span className="text-xs mt-1">{item.text}</span>
         </Link>
       ))}
